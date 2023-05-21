70b6fe59dd53dcffbc62ffc9f79d601e741b7dbb
Heshan93 <heshanrajitha1@gmail.com>
2023-05-21 22:09:14 +0530

    Load Background for seen 3

--- js/scene3.js
+++ js/scene3.js
@@ -1,261 +1,208 @@
+window.onload = init();
 
-  window.onload = init();
+function init() {
+  var cnv = document.getElementById("scene3");
+  var ctx = cnv.getContext("2d");
+  ctx.fillStyle = "white"; // set background to white
+  ctx.rect(0, 0, 1200, 749);
+  ctx.fill();
 
-  function init()
-  {
+  //////////Loading Image Object & image draw function  for 2st seen//////////////////
+
+  var backgroundimage = new Image(); //make image object
+  backgroundimage.src = "./Images/mountain.jpeg"; // set the image file path
+
+  // Coordinates and speed of Hero (Player)
+  var hx = 0; // Hero X coordinate
+  var hy = 0; // Hero Y coordinate
+  var SPEED = 5;
+
+  // Coordinates of Bat (Monster)
+  var cx = 850; // Bat X coordinate
+  var cy = 0; // Bat y coordinate
+
+
+
+  var heroStatus = true; // Hero doesn't move
+
+  function bat() {
+    var batFly = new Image(); //make image object for Bat
+    batFly.src = "./Images/bat.png"; // set the image file path for Bat
+    ctx.drawImage(batFly, cx, cy, 200, 100); // Image Height and Width for Bat
+  }
+
+
+  function heroUp() {
+    var heroUp = new Image(); //make image object for pirateUp
+    heroUp.src = "./Images/hero-scene1.png"; // set the image file path for pirateUp
+    ctx.drawImage(heroUp, hx, hy, 50, 100); // Image Height and Width for pirateUp
+  }
+
+
+  function heroFire() {
+    var heroFire = new Image(); //make image object for pirateUp
+    heroFire.src = "./Images/hero with fire.png"; // set the image file path for pirateUp
+    ctx.drawImage(heroFire, hx, hy, 100, 200); // Image Height and Width for pirateUp
+  }
+
+  function heroDead() {
+    var heroDead = new Image(); //make image object for pirateUp
+    heroDead.src = "./Images/images.jpeg"; // set the image file path for pirateUp
+    ctx.drawImage(heroDead, hx - 47, hy - 5, 170, 220); // Image Height and Width for pirateUp
+  }
+
+
+
+  function dungeon() {
+    var pirateDead = new Image(); //make image object for pirateUp
+    pirateDead.src = "./Images/dungeon.png"; // set the image file path for pirateUp
+    ctx.drawImage(pirateDead, 900, 270, 500, 300); // Image Height and Width for pirateUp
+  }
+
+
+
+
+   /* ===================================================| Bat Movement controls |=================================================== */
+
+
+  function batmove() {
+
     
-      var cnv = document.getElementById("scene3"); 
-      var ctx = cnv.getContext("2d"); 
-      ctx.fillStyle = "white";   // set background to white
-      ctx.rect(0,0,1200,749);
-      ctx.fill();
-
-      //////////Loading Image Object & image draw function  for 1st seen//////////////////
-
-      var backgroundimage = new Image(); //make image object
-      backgroundimage.src = "./Images/Dongen.webp"; // set the image file path
-
-    
-      // Coordinates and speed of Hero (Player)
-      var hx = 10;  // Hero X coordinate
-      var hy = 350; // Hero Y coordinate
-      var SPEED = 5;
-
-      // Coordinates of Cyclope (Monster)
-      var cx = 280; // Cyclop X coordinate 
-      var cy = 160;// Cyclop y coordinate 
-
-      // Coordinates of the Rock
-      rx = 350;
-      ry = 325;
-
-
-      var heroStatus = false; // Hero doesn't move
-
-      var cyclopStatus = false;// Cyclop sleep
-
-      // initialize visibility duration count
-      var count = 0;
-
-      hourcount = 0;
-
-
-      function CyclopSleep()
-      {
-            var CyclopImageSleep = new Image(); //make image object for Cyclop
-            CyclopImageSleep.src = "./Images/monster-sleeping.png"; // set the image file path for Cyclop
-            ctx.drawImage(CyclopImageSleep, cx, cy, 400,208); // Image Height and Width for Cyclop
-      }
-
-      function CyclopWake()
-      {
-            var CyclopImagWake = new Image(); //make image object for Cyclop left
-            CyclopImagWake.src = "./Images/monster-awaken.png"; // set the image file path for Cyclop left
-            ctx.drawImage(CyclopImagWake, cx, cy, 400,208); // Image Height and Width for Cyclop left 
-      }
-
-      function CyclopCommand()
-      {
-            var CyclopImgCommand = new Image(); //make image object for Cyclop left
-            CyclopImgCommand.src = "./Images/monster-stop.png"; // set the image file path for Cyclop left
-            ctx.drawImage(CyclopImgCommand, cx, cy, 400,208); // Image Height and Width for Cyclop left 
-      }
-
-      function rock()
-      {
-            var rockImage = new Image(); //make image object for Cyclop left
-            rockImage.src = "./Images/rock.png"; // set the image file path for Cyclop left
-            ctx.drawImage(rockImage, rx, ry, 250, 200); // Image Height and Width for Cyclop left 
-      }
-
-      function heroDown()
-      {
-            var heroDown = new Image(); //make image object for pirateUp
-            heroDown.src = "./Images/hero-scene1.png"; // set the image file path for pirateUp
-            ctx.drawImage(heroDown, hx, hy,100,200); // Image Height and Width for pirateUp
-          
-      }
-
-      function heroUp()
-      {
-            var heroUp = new Image(); //make image object for pirateUp
-            heroUp.src = "./Images/hero-scene1.png"; // set the image file path for pirateUp
-            ctx.drawImage(heroUp, hx, hy, 100,200); // Image Height and Width for pirateUp
-      }
-            
-      function heroDead()
-      {
-            var heroDead = new Image(); //make image object for pirateUp
-            heroDead.src = "./Images/hero-dead-scene1.png"; // set the image file path for pirateUp
-            ctx.drawImage(heroDead, hx-47, hy-5, 170,220); // Image Height and Width for pirateUp
-      }
-
-      function cantSee()
-      {
-            var pirateDead = new Image(); //make image object for pirateUp
-            pirateDead.src = "./Images/cant-see.png"; // set the image file path for pirateUp
-            ctx.drawImage(pirateDead, hx+100, hy, 200,70); // Image Height and Width for pirateUp
-      }
-
-      function dungeon()
-      {
-            var pirateDead = new Image(); //make image object for pirateUp
-            pirateDead.src = "./Images/dungeon.png"; // set the image file path for pirateUp
-            ctx.drawImage(pirateDead, 900, 270, 500,300); // Image Height and Width for pirateUp
-      }
-
-      function shootHero()
-      {
-          ctx.lineWidth = 8;
-          ctx.strokeStyle = 'red';
-          ctx.moveTo(505,210);
-          ctx.lineTo(hx+35,hy+100);
-          ctx.stroke();
-          var demoImage = new Image();   // make image object
-          demoImage.src = "./Images/blood.png";  // set image path
-          ctx.drawImage(demoImage, hx-25, hy+10, 150, 150);  
-      }
-
-    /* ===================| Loading Image Object & image draw function for 1st seen | =================== */
-
-
-    /* ===================| Visibility | =================== */
-    function checkVisibility()
-    {
-      if ((hx < 350) || (hx > 530)){
-        EnemyCanSee = true;
-      }else{
-        EnemyCanSee = false;
-        cantSee();
-      } 
+    var dx = hx - cx; // Calculate the difference in X positions
+    var dy = hy - cy; // Calculate the difference in Y positions
+    var distance = Math.sqrt(dx * dx + dy * dy); // Calculate the distance between the bat and the hero
+  
+    var angle = Math.atan2(dy, dx);
+  
+    // Calculate the movement speed of the bat
+    var speed = 2;
+  
+    if (heroStatus == "fire" && distance < 500) {
+      // If heroStatus is "fire" and bat is near the hero
+      // Move the bat away from the hero
+      speed *= -1; // Reverse the direction of movement
+    }else if (distance < 100) {
+      // If bat is near the hero without heroStatus being "fire"
+      // Set heroStatus to false
+    heroStatus = false;
     }
+  
+    // Calculate the movement in X and Y directions
+    var moveX = speed * Math.cos(angle);
+    var moveY = speed * Math.sin(angle);
+  
+    // Update the bat's position
+    cx += moveX;
+    cy += moveY;
+  }
+
+
 
 
   /* ===================================================| Handle Keyboard controls |=================================================== */
 
-  var keyPress = {} ;   // initialize the  key presses
-  addEventListener("keydown",  function (e) 
-  {
-    keyPress[e.keyCode] = true; // If press True
-    heroStatus = true;   // to change pirate image 
-  }, false);
-
-  addEventListener("keyup",   function (e)  
-  {
-    delete keyPress[e.keyCode];
-      heroStatus = false; // to change pirate image 
-  }, false);
+  var keyPress = {}; // initialize the  key presses
+  addEventListener(
+    "keydown",
+    function (e) {
+      keyPress[e.keyCode] = true; // If press True
+     
+      if (e.keyCode === 32 && heroStatus !== false) {
+        // If the space bar is pressed
+        heroStatus = "fire"; // Set heroStatus to "fire"
+      }
 
 
+      console.log(e.keyCode);
+    },
+    false
+  );
 
+  addEventListener(
+    "keyup",
+    function (e) {
+      delete keyPress[e.keyCode];
+     
+      if (e.keyCode === 32) {
+        // If the space bar is released
+        heroStatus = true; // Set heroStatus back to true
+      }
+
+    },
+    false
+  );
 
   /* ===================================================| Hero moving controls |=================================================== */
-  function heroMove()
-  {
+  function upDate() {
     
 
-      checkVisibility();
+    if (!heroStatus) {
+      return; // // Stop moving if the hero is dead 
+    }
 
-      if(37 in keyPress){ // left arrorw
-        hx =  hx - SPEED;
-      }
     
-      if (39 in keyPress){  // right arrow
-        hx = hx + SPEED;
+    if (37 in keyPress) {
+      // left arrorw
+      if (hx - SPEED >= 0) { // Check if moving left won't exceed left boundary
+        hx -= SPEED;
       }
+    }
 
-      if (EnemyCanSee == true && cyclopStatus == true){
-        //enemyCOLOR = "red";
-        count = count + 1; 
-      } else {
-        count = 0;
+    if (39 in keyPress) {
+      // right arrow
+      if (hx + SPEED <= cnv.width - 100) { // Check if moving right won't exceed right boundary
+        hx += SPEED;
       }
+    }
 
-      if (count > 40) {  //player was visible for 3 seconds
-        CyclopCommand();
-        //shootHero(); 
-      }
-      if (count > 80) {  //player was visible for 3 seconds
-        //CyclopCommand();
-        shootHero(); 
-        heroStatus = "dead";
-      }
-
-      
+    if(hx>1090){ // Loads the seen 2 
+      window.open("./index3.html", "_self");
+    }
+    
   }
   /* ===================================================| / Hero moving controls |=================================================== */
 
-
-
-
-  function clear()
-  {
+  function clear() {
     ctx.drawImage(backgroundimage, 0, 0, cnv.width, cnv.height); // Image Height and Width
-    //ctx.drawImage(shootRemove, 0, 0, cnv.width, cnv.height); // Image Height and Width
-    
-  }
-
-
-
-      // Call set cyclopStatus  every 3 seconds
-      setInterval(function() {
-        if (Math.random() < 0.5) {
-          cyclopStatus = true;
-        } else {
-          cyclopStatus = false;
-        }
-      }, 3000);
-    
-
-  function draw()
-  {
-    dungeon();
    
-      if(cyclopStatus == true)
-      {
-        CyclopWake();
-      }
-      else
-      {
-        CyclopSleep();
-      }
-
-      
-      rock();
-      
-      if(heroStatus == true)
-        {
-          heroUp();
-        }
-        else if(heroStatus == "dead")
-        {
-          heroDead();
-      } else{
-        heroDown();
-      }
-
-        
-
-
   }
 
+  // Call set BatStatus  every 3 seconds
+  setInterval(function () {
+    if (Math.random() < 0.5) {
+      BatStatus = true;
+    } else {
+      BatStatus = false;
+    }
+  }, 3000);
 
+
+
+
+  function draw() {
+    dungeon();
+
+    bat();
+
+
+    if (heroStatus == true) {
+      heroUp();
+    } else if (heroStatus == false) {
+      heroDead();
+    } else if (heroStatus == "fire") {
+      heroFire();
+    }
+
+  }
 
   function Loop() {
     clear();
     draw();
-    heroMove();
-    
-    hourcount = hourcount+1;
-    
-    setTimeout(Loop, 20);  //call loop every 20 mili sec 
+    upDate();
+    batmove();
 
+    setTimeout(Loop, 20); //call loop every 20 mili sec
   } //end loop
 
-
-
-
-
-  Loop()
-
-  }
+  Loop();
+}
